894f902a7246c77e2974af585e6c4c2c035957c3
vanakkamtechie <68475441+vanakkamtechie@users.noreply.github.com>
2020-07-20 12:41:04 +0530

    Update scratchpad.go

--- scratchpad.go
+++ scratchpad.go
@@ -1,1 +1,16 @@
-test file
+package main
+
+import "fmt"
+
+// Add adds two integers
+func Add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	var i, j = 42, 50
+
+	fmt.Println("Hello World! Result: ", Add(i, j))
+
+	fmt.Println(&i, &j)
+}
