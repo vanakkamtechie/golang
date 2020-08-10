ca61206832f12897ae646db4705edd1a95411caf
Balakrishnan Nainar <bnainar@cisco.com>
2020-08-10 11:48:39 +0530

    sample code

--- examples/pointers/sample2.go
+++ examples/pointers/sample2.go
@@ -11,6 +11,21 @@ func main() {
 	var v *Vertex
 	fmt.Printf("Vertex %p \n", v)
 
-	var p = *new(Vertex)
-	fmt.Printf("Vertex %p \n", p)
+	var p = new(Vertex)
+	p.X = 10
+	fmt.Println(p.X)
+
+	p1 := p
+	p1.X = 20
+
+	fmt.Println(p.X)
+
+	changeXValue(p1)
+
+	fmt.Println(p.X)
+
+}
+
+func changeXValue(vert *Vertex) {
+	vert.X = 100
 }
